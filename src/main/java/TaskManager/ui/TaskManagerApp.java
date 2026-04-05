e9d002ae9da62ec4e2bb4100af919e0833137984
Jakob <zippererjakob@gmail.com>
2026-04-05 18:38:43 +0200

    Setup JavaFX UI with Maven, initial GUI working

---
+++ src/main/java/TaskManager/ui/TaskManagerApp.java
@@ -0,0 +1,28 @@
+package TaskManager.ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TaskManagerApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("JavaFX läuft!");
+
+        VBox root = new VBox();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 400, 200);
+
+        stage.setTitle("Task Manager");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
